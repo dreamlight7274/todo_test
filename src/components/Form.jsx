7317a4858160397128394165a6a97dfe82f8be04
dreamlight7274 <dreamlight7274@gmail.com>
2024-04-01 15:59:45 +0100

    recognize
    recognize

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,65 @@
 import { useEffect, useState } from "react";
+import "regenerator-runtime/runtime";
+import { useSpeechRecognition } from "react-speech-recognition";
+import SpeechRecognition from "react-speech-recognition";
+
+function SpeechRecoginizecreate() {
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [recognitionResult, setRecognitionResult] = useState("");
+
+  const startListen = () => {
+    if (window.hasOwnProperty("webkitSpeechRecognition")) {
+      const recognition = new window.webkitSpeechRecognition();
+      recognition.continuous = false;
+      recognition.interimResults = true;
+      recognition.lang = "en-UK";
+      recognition.start();
+
+      recognition.onresult = (e) => {
+        const bestResult = e.results[0][0].transcript;
+        resetTranscript();
+        console.log("Speech Result:", bestResult);
+        // useEffect(() => {
+        //   setrecognitionResult(bestResult);
+        // }, [bestResult]);
+        setRecognitionResult(bestResult);
+      };
+
+      recognition.onerror = (event) => {
+        console.error("Speech Recognition Error:", event.error);
+      };
+    } else {
+      console.error("The browser doesn't support webkitSpeechRecognition");
+    }
+  };
+  const stopListen = () => {
+    SpeechRecognition.stopListening();
+  };
+  const cleanResult = () => {
+    setRecognitionResult(null);
+  };
+
+  // return recognitionResult;
+
+  // return (
+  //   <div>
+  //     <button onClick={startListen}>Start Listening</button>
+  //     <button onClick={stopListen}>Stop Listening</button>
+  //     <p>Transcript: {recognitionResult}</p>
+  //   </div>
+  // );
+  return {
+    startListen,
+    stopListen,
+    cleanResult,
+    recognitionResult,
+  };
+}
 function Form({ addTask, FindMe }) {
   const [name, setName] = useState("Learn React");
   const [addition, setAddition] = useState(false);
+    const { startListen, stopListen, cleanResult, recognitionResult } =
+    SpeechRecoginizecreate();
 
   useEffect(() => {
     if (addition) {
@@ -10,6 +68,11 @@ function Form({ addTask, FindMe }) {
       setAddition(false);
     }
   });
+    useEffect(() => {
+    if (recognitionResult !== null) {
+      setName(recognitionResult);
+    }
+  }, [recognitionResult]); // use effect, if recognitionResult is not null
   // a trigger to make the locating work
   //   function handleChange() {
   //     console.log("Typing!");
@@ -46,9 +109,23 @@ function Form({ addTask, FindMe }) {
         className="input input__lg"
         name="text"
         autoComplete="off"
-        value={name}
+        value={recognitionResult || name}
         onChange={handleChange}
       />
+            <div className="btn-group">
+        <button type="button" className="btn" onClick={startListen}>
+          {" "}
+          start recognize{" "}
+        </button>
+        <button type="button" className="btn" onClick={stopListen}>
+          {" "}
+          stop recognize{" "}
+        </button>
+        <button type="button" className="btn__danger" onClick={cleanResult}>
+          {" "}
+          just input{" "}
+        </button>
+      </div>
       <button type="submit" className="btn btn__primary btn__lg">
         Add
       </button>
