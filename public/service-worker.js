cbd96ba6de61d731d092154acca69c394350f51c
dreamlight7274 <dreamlight7274@gmail.com>
2024-04-06 00:03:28 +0100

    may be need FCM
    maybe need FCM

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,3 @@
 self.addEventListener("install", (event)=>{
     console.log("Service worker installing...");
 });
-self.addEventListener('push', function(event) {
-  const data = event.data.json();
-
-  const options = {
-    body: data.body,
-    icon: 'logo192.png', 
-
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
-  );
-});
